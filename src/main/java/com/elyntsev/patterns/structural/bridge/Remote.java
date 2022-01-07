9dbdfb636dd7f7b4938c629262d2b6e8c17f4985
Kirill <kirill.elyntsev@gmail.com>
2022-01-07 17:20:48 +0300

    added bridge

---
+++ src/main/java/com/elyntsev/patterns/structural/bridge/Remote.java
@@ -0,0 +1,14 @@
+package com.elyntsev.patterns.structural.bridge;
+
+public interface Remote {
+
+    void power();
+
+    void volumeDown();
+
+    void volumeUp();
+
+    void channelDown();
+
+    void channelUp();
+}
